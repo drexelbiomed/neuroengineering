ba8533a81412c43d39052589b5a40940749f0619
Dave Myers <drm68@drexel.edu>
2020-07-22 07:20:06 -0400

    added js, fixed broken reference to http_prefix

---
+++ source/javascripts/foundation-3.js
@@ -0,0 +1,7 @@
+//= require jquery-1.11.1/index
+//= require foundation-3/javascripts/foundation.min
+//= require bxslider-4/jquery.bxslider.js
+
+//= require foundation-3/javascripts/app
+//= require bxslider
+//= require script
